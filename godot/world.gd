c65499fc4d46142937661eb11d9e6541376df9b0
Paul Norberger <hi@paulnorberger.de>
2024-04-13 16:31:22 +0200

    Remove semicolons

--- godot/world.gd
+++ godot/world.gd
@@ -8,26 +8,26 @@ var scenes := {
 	'main_menu': main_menu_scene,
 }
 
-@onready var current_scene_node: Node = null;
+@onready var current_scene_node: Node = null
 @onready var current_scene_key: String 
 @onready var current_scene: String:
 	get:
-		return current_scene_key;
+		return current_scene_key
 	set(value):
-		self.remove_child(self.current_scene_node);
-		var next_scene: PackedScene = scenes[value];
-		var next_scene_node: Node = next_scene.instantiate();
-		self.add_child(next_scene_node);
-		self.current_scene_key = value;
-		self.current_scene_node = next_scene_node;
+		self.remove_child(self.current_scene_node)
+		var next_scene: PackedScene = scenes[value]
+		var next_scene_node: Node = next_scene.instantiate()
+		self.add_child(next_scene_node)
+		self.current_scene_key = value
+		self.current_scene_node = next_scene_node
 		
 		if next_scene_node is MainMenu:
-			var main_menu_node = next_scene_node as MainMenu;
+			var main_menu_node = next_scene_node as MainMenu
 			main_menu_node.start_game.connect(_on_game_start)
 			
 
 func _ready():
-	self.current_scene = start_scene;
-	
+	self.current_scene = start_scene
+
 func _on_game_start():
-	print('start game');
+	print('start game')
