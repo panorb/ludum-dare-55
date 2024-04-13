5e30a9ae47fbd26cd94066860869f55843bdd74c
Paul Norberger <hi@paulnorberger.de>
2024-04-13 19:10:48 +0200

    Try figuring out vieport sizes

--- godot/game/game.gd
+++ godot/game/game.gd
@@ -1,19 +1,17 @@
-extends Node2D
+extends Control
 
-@onready var sub_viewport : SubViewport = get_node("%SubViewport")
-@onready var move_area : Area2D = get_node("MoveArea")
+@onready var environment_viewport := %EnvironmentViewport
+@onready var level_viewport := %LevelViewport
 
 func _ready():
 	_on_window_size_changed()
 	get_tree().get_root().size_changed.connect(_on_window_size_changed)
-	move_area.body_entered.connect(_on_move_area_body_entered)
-	move_area.body_exited.connect(_on_move_area_body_exited)
 
 func _on_window_size_changed():
-	sub_viewport.size = get_tree().get_root().size
+	environment_viewport.size = get_tree().get_root().size
+	
+	print(get_tree().get_root().size)
+	level_viewport.size.x = (320.0 / 1280.0) * get_tree().get_root().size.x
+	level_viewport.size.y =  (180.0 / 720.0) * level_viewport.size.x
+	print(level_viewport.size)
 
-func _on_move_area_body_entered(body: Node2D) -> void:
-	pass
-
-func _on_move_area_body_exited(body: Node2D) -> void:
-	pass
--- godot/game/level.gd
+++ godot/game/level.gd
@@ -1,2 +1,4 @@
 extends Node2D
 
+func _update(delta: float):
+	pass
