ccec736c65f780db5ee2af3f809d63759c41f475
Paul Norberger <hi@paulnorberger.de>
2024-04-13 21:11:14 +0200

    fuck some shit

--- godot/game/game.gd
+++ godot/game/game.gd
@@ -1,11 +1,37 @@
 extends Control
 
+@onready var level_viewport := %LevelViewport
 @onready var environment_viewport := %EnvironmentViewport
+@onready var environment := %Environment
+@onready var level := %Level
 
+var run_no = 0
+
+func _process(delta):
+	var player_pos = level.get_player_position()
+	print(player_pos)
+	if abs(player_pos) > 0.6 and run_no < 17:
+		environment.move_view(player_pos)
+		level.move_view(player_pos)
+		run_no += 1
+
+func _input(event):
+	pass
+	#if event is InputEventMouseButton:
+	#	if event.pressed:
+	#		print(level_viewport.get_mouse_position())
 
 func _ready():
 	_on_window_size_changed()
 	get_tree().get_root().size_changed.connect(_on_window_size_changed)
+	level.viewport_size = level_viewport.size
+	
 
 func _on_window_size_changed():
 	environment_viewport.size = get_tree().get_root().size
+
+
+func _on_level_texture_gui_input(event):
+	if event is InputEventMouseButton:
+		if event.pressed:
+			print(event.position)
--- godot/game/level.gd
+++ godot/game/level.gd
@@ -1,4 +1,18 @@
 extends Node2D
 
+var viewport_size = Vector2.ZERO
+@onready var player := $Player
+
 func _update(delta: float):
 	pass
+
+func get_player_position():
+	var half_length = viewport_size.x/2
+	var px_center = position.x + half_length
+	var px_offset = player.position.x - px_center
+	var relative_offset = px_offset / half_length
+	return relative_offset
+	# return 
+
+func move_view(move_x: float):
+	position.x -= move_x * 10.0
