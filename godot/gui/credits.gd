58b6f4d762671ffce816f59f09707b05c16d73f6
Toni Beier <toni.beier@hrz.tu-chemnitz.de>
2024-04-14 19:19:26 +0200

    Fix displaying credits

--- godot/gui/credits.gd
+++ godot/gui/credits.gd
@@ -9,8 +9,16 @@ func _ready():
 	if credits_persons.data is Dictionary:
 		var persons = credits_persons.data as Dictionary
 		for task_key in persons:
-			var persons_name := PackedStringArray(persons[task_key])
-			var credits_task_instance := credits_task.instantiate()
-			credits_task_instance.task_text = task_key
-			credits_task_instance.persons = persons_name
-			main_v_box_container.add_child(credits_task_instance)
+			var person_names := PackedStringArray(persons[task_key])
+			
+			var task_rich_text_label = RichTextLabel.new()
+			task_rich_text_label.text = "[center][b]%s[/b][/center]" % tr(task_key)
+			task_rich_text_label.bbcode_enabled = true
+			task_rich_text_label.fit_content = true
+			main_v_box_container.add_child(task_rich_text_label)
+			
+			var person_names_rich_text_label := RichTextLabel.new()
+			person_names_rich_text_label.text = "[center]%s[/center]\n " % " | ".join(person_names)
+			person_names_rich_text_label.bbcode_enabled = true
+			person_names_rich_text_label.fit_content = true
+			main_v_box_container.add_child(person_names_rich_text_label)
