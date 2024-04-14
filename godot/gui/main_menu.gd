7e21eec7ba3c5ef77a65bf5503c7e7e73b5765d5
Toni Beier <toni.beier@hrz.tu-chemnitz.de>
2024-04-14 23:24:50 +0200

    Add sound slider for sfx sound

--- godot/gui/main_menu.gd
+++ godot/gui/main_menu.gd
@@ -5,22 +5,30 @@ signal start_game
 @onready var start_button := %StartButton
 @onready var credits_button := %CreditsButton
 @onready var quit_button := %QuitButton
-@onready var sound_slider := %SoundSlider
-@onready var sound_percent_label := %SoundPercentLabel
 
-@onready var master_bus_name := 'Master'
+@onready var sfx_sound_slider := %SfxSoundSlider
+@onready var sfx_sound_percent_label := %SfxSoundPercentLabel
+
+@onready var music_sound_slider := %MusicSoundSlider
+@onready var music_sound_percent_label := %MusicSoundPercentLabel
+
+@onready var sfx_bus_name := 'SFX'
+@onready var music_bus_name := 'Music'
+
 const credits_scene := preload("res://gui/credits.tscn")
 
-func _ready():	
+func _ready():
 	start_button.pressed.connect(self._on_start_button_pressed);
 	credits_button.pressed.connect(self._on_credits_button_pressed)
 	quit_button.pressed.connect(self._on_quit_button_pressed);
-	sound_slider.value_changed.connect(self._on_sound_slider_value_changed);
-	
+	sfx_sound_slider.value_changed.connect(self._on_sfx_sound_slider_value_changed)
+	music_sound_slider.value_changed.connect(self._on_music_sound_slider_value_changed)
+
 	if OS.has_feature('web'):
 		quit_button.visible = false
-		
-	sound_slider.value = 80;
+
+	sfx_sound_slider.value = 80
+	music_sound_slider.value = 80
 
 func _on_credits_button_pressed() -> void:
 	var credits := credits_scene.instantiate()
@@ -28,14 +36,20 @@ func _on_credits_button_pressed() -> void:
 
 func _on_start_button_pressed() -> void:
 	start_game.emit()
-	
+
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
 
-func _on_sound_slider_value_changed(volume_value: float) -> void:
-	var master_bus_index := AudioServer.get_bus_index('Master')
-	AudioServer.set_bus_volume_db (master_bus_index, linear_to_db(volume_value / 100))
-	sound_percent_label.text = "  %d %%" % volume_value
+func _on_sfx_sound_slider_value_changed(volume_value: float) -> void:
+	set_bus_volume(sfx_bus_name, volume_value, sfx_sound_percent_label)
+
+func _on_music_sound_slider_value_changed(volume_value: float) -> void:
+	set_bus_volume(music_bus_name, volume_value, music_sound_percent_label)
+
+func set_bus_volume(bus_name: String, volume_percent: float, label_node: Label ):
+	var bus_index := AudioServer.get_bus_index(bus_name)
+	AudioServer.set_bus_volume_db (bus_index, linear_to_db(volume_percent / 100))
+	label_node.text = "%d %%" % volume_percent
 
 	#if volume_value <= 0 :
 	#	sound_image.texture = volume_mute_image
