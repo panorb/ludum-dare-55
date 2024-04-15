74f402f87e8befaaae72e73948c25c1cb2ddb502
Toni Beier <toni.beier@hrz.tu-chemnitz.de>
2024-04-15 12:52:26 +0200

    Add user info for restarting in lose screen

--- godot/gui/end_screen/lose_screen.gd
+++ godot/gui/end_screen/lose_screen.gd
@@ -2,6 +2,8 @@ class_name LoseScreen
 
 extends Control
 
+@onready var restart_info := %RestartInfo
+
 signal show_main_menu
 
 # Called when the node enters the scene tree for the first time.
@@ -20,3 +22,9 @@ func _ready():
 	tween.tween_property($Halo, "position", Vector2($Wings.position.x, $Wings.position.y - 8), 2.0).set_trans(Tween.TRANS_SINE)
 	tween.tween_property($Halo, "position", Vector2($Wings.position.x, $Wings.position.y + 8), 2.0).set_trans(Tween.TRANS_SINE)
 	tween.set_loops()
+
+	var restart_info_tween_transparency := Color(Color.WHITE, 0.4)
+	var restart_info_tween = create_tween()
+	restart_info_tween.tween_property(restart_info, "modulate", restart_info_tween_transparency, 0.8)
+	restart_info_tween.tween_property(restart_info, "modulate", Color.WHITE, 0.8)
+	restart_info_tween.set_loops()
