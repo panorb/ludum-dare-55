48db09829d0fd194af9b9d85bf059d0bf023dd51
Paul Norberger <hi@paulnorberger.de>
2024-04-15 19:07:21 +0200

    Write laser targeting logic

--- godot/game/entities/entity.gd
+++ godot/game/entities/entity.gd
@@ -5,10 +5,12 @@ signal collision(obstacle)
 
 var damage = 50
 var entered_screen_once = false
+var player = null
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	body_entered.connect(on_body_entered)
+	player = get_tree().get_first_node_in_group("player")
 
 func _move(delta):
 	pass
